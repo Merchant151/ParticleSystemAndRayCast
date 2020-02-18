c5b6a77aa817fb19ea652ece00dc080daaf0bc87
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2020-02-18 15:48:07 -0500

    imported assets that will be used

---
+++ ParticleSystemAndRayCast/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 step = transform.forward * v + transform.right * h;
+        transform.position += step * 5 * Time.deltaTime;
+
+    }
+}
