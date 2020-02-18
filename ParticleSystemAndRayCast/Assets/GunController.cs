6665aebbcd09ab2ce95fd5658ec80e58353bbaf5
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2020-02-18 16:34:37 -0500

    created snow using particle system

---
+++ ParticleSystemAndRayCast/Assets/GunController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    Transform playerTransform;
+    
+    void Start()
+    {
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(playerTransform.position);
+        //transform.forward += Vector3.up * 10;
+    }
+}
